8bd1e637512a4c56474eb01c953901ea5ebb8c76
WEDEN <62192360+wedengabriel@users.noreply.github.com>
2025-09-02 11:07:02 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,4 @@ app = FastAPI()
 @app.get("/hello")
 def hello_world():  # função sem docstring
     return {"mensagem": "Olá, mundo! Esta mensagem ultrapassa os 90 caracteres para testar o linting"}
+#teste
