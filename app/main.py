bc096209c0334cfec1f5fdf27358278fd2ecad30
Cleberton Silva <cleberton.silva@quipux.com.br>
2025-09-02 12:05:14 -0300

    feat: teste

--- app/main.py
+++ app/main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/hello")
-def hello_world():  # função sem docstring e linha longa proposital para violar as regras
+def hello_world():  # função sem docstring
     return {"mensagem": "Olá, mundo! Esta mensagem ultrapassa os 90 caracteres para testar o linting"}
