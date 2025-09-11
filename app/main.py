4d6e9f954f7fef434776adcce682954e95e2c1c0
desyreegarcia <desyree.garcia@hotmail.com>
2025-09-11 20:38:25 -0300

    RotaPedidos

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 """Módulo principal da aplicação EasyOrder com FastAPI."""
 
 from fastapi import FastAPI
+from app.routers import pedidos
+
 
 app = FastAPI()
-
+app.include_router(pedidos.router)
 
 @app.get("/hello")
 def hello_world():
